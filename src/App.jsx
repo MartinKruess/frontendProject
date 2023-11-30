7b6ea53e8b50a0fd5719dc52a217c28ecb039e61
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2023-11-30 15:21:02 +0100

    vorlage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
-  return <>test</>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:5501/users");
+      const data = await res.json();
+      console.log(data);
+      setUsers(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {users.map((user) => (
+        <p key={user.id}>{user.firstname}</p>
+      ))}
+    </>
+  );
 }
 
 export default App;
